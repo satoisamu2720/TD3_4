5237a9dfe84f171db8384629a2592db5adf01275
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-04-10 16:23:07 +0900

    コード保存

--- DirectXGame/rain/Rain.cpp
+++ DirectXGame/rain/Rain.cpp
@@ -3,7 +3,28 @@
 void Rain::Initalize(const std::vector<Model*>& models) { 
 	BaseCharacter::Initialize(models);
 
+	worldTransform_[0].scale_ = {3.0f, 3.0f, 3.0f};
+	worldTransform_[0].rotation_ = {0.0f, 10.0f, 0.0f};
+	worldTransform_[0].translation_ = {0.0f, 4.0f, 100.0f};
+
+	worldTransform_[1].scale_ = {3.0f, 3.0f, 3.0f};
+	worldTransform_[1].rotation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_[1].translation_ = {10.0f, 4.0f, 150.0f};
+
+	worldTransform_[2].scale_ = {3.0f, 3.0f, 3.0f};
+	worldTransform_[2].rotation_ = {0.0f, -2.0f, 0.0f};
+	worldTransform_[2].translation_ = {50.9f, 4.0f, 120.0f};
+
+
+
+
 	  for (int i = 0; i < 20; i++) {
+
+
+
+
+
+
 
 		
 		worldTransform_[i].Initialize();
@@ -15,7 +36,18 @@ void Rain::Update() {
 
 	
 
+
+
+
+
 	for (int i = 0; i < 20; i++) {
+
+
+		worldTransform_[i].translation_.x += target_->translation_.x;
+		worldTransform_[i].translation_.y += target_->translation_.y;
+
+
+
 
 		worldTransform_[i].translation_.z -= 0.1f;
 
