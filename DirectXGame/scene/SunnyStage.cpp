b5729275677e4b28189377da2ee621c6514b4d61
裕斗 近藤 <hilotono@icloud.com>
2024-05-16 15:45:59 +0900

    障害物変更

--- DirectXGame/scene/SunnyStage.cpp
+++ DirectXGame/scene/SunnyStage.cpp
@@ -44,7 +44,7 @@ void SunnyStage::Initialize() {
 #pragma region 障害物
 
 	// 箱モデル読み込み
-	BoxModel_ = (Model::CreateFromOBJ("woodenBox", true));
+	BoxModel_ = (Model::CreateFromOBJ("colorCorn", true));
 	// ボックスのCSVファイル読み込み
 	LoadBoxPopData();
 
