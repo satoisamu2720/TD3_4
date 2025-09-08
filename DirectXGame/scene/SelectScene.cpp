5c6911d6d73dda9ee9312840956882de09382c26
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2025-09-08 11:50:44 +0900

    ok

--- DirectXGame/scene/SelectScene.cpp
+++ DirectXGame/scene/SelectScene.cpp
@@ -10,254 +10,37 @@ void SelectScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 
-	worldTransformSunny_.Initialize();
+	backSceneTexHandle_ = TextureManager::Load("SelectScene.png");
+	backSceneSprite_ = Sprite::Create(backSceneTexHandle_, {0, 0});
 
-	worldTransformRain_.Initialize();
+	testSceneTexHandle_ = TextureManager::Load("Mirror.png");
+	testSceneSprite_ = Sprite::Create(testSceneTexHandle_, {320, 360});
 
-	worldTransformSnow_.Initialize();
+	gameSceneTexHandle_ = TextureManager::Load("Mirror.png");
+	gameSceneSprite_ = Sprite::Create(gameSceneTexHandle_, {960, 360});
 
-	worldTransformFog_.Initialize();
+	tutorialTexHandle_ = TextureManager::Load("tutorial.png");
+	tutorialSprite_ = Sprite::Create(tutorialTexHandle_, {100, 400});
 
-	viewProjection_.Initialize();
-
-	modelSkydome_.reset(Model::CreateFromOBJ("Sky", true));
-
-	// スカイドームの生成と初期化
-	skydome_ = std::make_unique<Skydome>();
-	skydome_->Initialize(modelSkydome_.get(), {0, 0, 0});
-
-	modelCloud_.reset(Model::CreateFromOBJ("cloud", true));
-
-	// 雲の生成と初期化
-	cloud_ = std::make_unique<Cloud>();
-	cloud_->Initialize(modelCloud_.get(), true);
-	selectSwitchTimer = 120;
-
-	// 背景スプライト
-	// titleTexHandle_ = TextureManager::Load("sky.png");
-	// Sprite_ =
-	//    Sprite::Create(titleTexHandle_, {640, 360}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
-
-#pragma region モデル
-
-	sunModel_.reset(Model::CreateFromOBJ("sunMark", true));
-
-	rainModel_.reset(Model::CreateFromOBJ("rain", true));
-
-	snowModel_.reset(Model::CreateFromOBJ("Snowman", true));
-
-	fogModel_.reset(Model::CreateFromOBJ("fog", true));
-
-#pragma endregion
-
-#pragma region 初期ポジション
-
-	degree[SUNNY] = position_[SUNNY];
-
-	degree[RAIN] = position_[RAIN];
-
-	degree[SNOW] = position_[SNOW];
-
-	degree[FOG] = position_[FOG];
-
-#pragma endregion
-
-	rotf[SUNNY] = DirectX::XMConvertToRadians(degree[SUNNY]);
-
-	rotf[RAIN] = DirectX::XMConvertToRadians(degree[RAIN]);
-
-	rotf[SNOW] = DirectX::XMConvertToRadians(degree[SNOW]);
-
-	rotf[FOG] = DirectX::XMConvertToRadians(degree[FOG]);
-
-	worldTransformSunny_.scale_ = {2, 2, 2};
-
-	worldTransformRain_.scale_ = {2, 2, 2};
-
-	worldTransformSnow_.scale_ = {2, 2, 2};
-
-	worldTransformFog_.scale_ = {1, 1, 1};
-
-	// サウンド
-
-	BGM_ = Audio::GetInstance()->LoadWave("Sound/BGM.mp3");
-	cloudSound_ = Audio::GetInstance()->LoadWave("Sound/cloud.mp3");       // 雲
-	moveSound_ = Audio::GetInstance()->LoadWave("Sound/button06.mp3");     // ADボタン
-	decisionSound_ = Audio::GetInstance()->LoadWave("Sound/button01.mp3"); // 決定ボタン
-	summerSound_ = Audio::GetInstance()->LoadWave("Sound/summer.mp3");     // 晴BGM
-	gameOverSound_ = Audio::GetInstance()->Audio::LoadWave("Sound/gameOver.mp3");
-	gameClearSound_ = Audio::GetInstance()->Audio::LoadWave("Sound/gameClear.mp3");
-
-	// Audio::GetInstance()->Audio::PauseWave(BGM_);
+	stageTexHandle_ = TextureManager::Load("stage.png");
+	stageSprite_ = Sprite::Create(stageTexHandle_, {800, 400});
 }
 
 void SelectScene::Update() {
 
-	Audio::GetInstance()->Audio::ResumeWave(BGM_);
-
-	if (stageCount_ == 0 && summerON) {
-		// Audio::GetInstance()->Audio::PlayWave(summerSound_, true, 1.0f);
-		summerON = false;
+	if (input_->TriggerKey(DIK_LEFT)) {
+		selectCount = 0;
 	}
 
-	if (selectSwitchFlag) {
-		cloud_->SetMoveFlag(true);
-		selectSwitchTimer--;
-	}
-	if (selectSwitchTimer <= 0) {
-		cloud_->SetMoveFlag(false);
+	if (input_->TriggerKey(DIK_RIGHT)) {
+		selectCount = 1;
 	}
 
-	if (selectSwitchTimer == 119) {
-
-		Audio::GetInstance()->Audio::PlayWave(cloudSound_, false, 1.0f);
+	if (selectCount == 0 && input_->TriggerKey(DIK_SPACE)) {
+		sceneNo = END;
+	} else if (selectCount == 1 && input_->TriggerKey(DIK_SPACE)) {
+		sceneNo = CLEAR;
 	}
-	cloud_->Update();
-
-	XINPUT_STATE joyState;
-
-	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
-		if (joyState.Gamepad.sThumbLX < -100 && leftFlag_ == false && rightFlag_ == false) {
-			stageCount_--;
-			leftFlag_ = true;
-			Audio::GetInstance()->Audio::PlayWave(moveSound_, false, 1.0f);
-		}
-
-		if (joyState.Gamepad.sThumbLX > 100 && leftFlag_ == false && rightFlag_ == false) {
-			stageCount_++;
-			rightFlag_ = true;
-			Audio::GetInstance()->Audio::PlayWave(moveSound_, false, 1.0f);
-		}
-	}
-	if (input_->TriggerKey(DIK_LEFT) && leftFlag_ == false && rightFlag_ == false ||
-	    input_->TriggerKey(DIK_A) && leftFlag_ == false && rightFlag_ == false) {
-		stageCount_--;
-		leftFlag_ = true;
-		setFlag_ = false;
-		Audio::GetInstance()->Audio::PlayWave(moveSound_, false, 1.0f);
-	}
-
-	if (input_->TriggerKey(DIK_RIGHT) && leftFlag_ == false && rightFlag_ == false ||
-	    input_->TriggerKey(DIK_D) && leftFlag_ == false && rightFlag_ == false) {
-		stageCount_++;
-		rightFlag_ = true;
-		setFlag_ = false;
-		Audio::GetInstance()->Audio::PlayWave(moveSound_, false, 1.0f);
-	}
-
-	if (stageCount_ != 0) {
-		// Audio::GetInstance()->Audio::PauseWave(summerSound_);
-	} else {
-		// Audio::GetInstance()->Audio::ResumeWave(summerSound_);
-	}
-
-	if (stageCount_ > 3) {
-		stageCount_ = 0;
-	} else if (stageCount_ < 0) {
-		stageCount_ = 3;
-	}
-
-	if (leftFlag_ == true && rightFlag_ == false) {
-		target_++;
-
-		if (target_ >= 90) {
-			leftFlag_ = false;
-			target_ = 0;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			if (target_ != 90) {
-				degree[i] += 1;
-				rotf[i] = DirectX::XMConvertToRadians(degree[i]);
-			}
-		}
-	}
-
-	if (rightFlag_ == true && leftFlag_ == false) {
-		target_++;
-
-		if (target_ >= 90) {
-			rightFlag_ = false;
-			target_ = 0;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			if (target_ != 90) {
-				degree[i] -= 1;
-				rotf[i] = DirectX::XMConvertToRadians(degree[i]);
-			}
-		}
-	}
-
-	if (input_->TriggerKey(DIK_SPACE) && leftFlag_ == false && rightFlag_ == false) {
-		sceneNo = stageNo_[stageCount_];
-
-		Audio::GetInstance()->Audio::PlayWave(decisionSound_, false, 1.0f);
-	}
-	if (input_->TriggerKey(DIK_H)) {
-
-		Audio::GetInstance()->Audio::PauseWave(summerSound_);
-	}
-	if (input_->TriggerKey(DIK_SPACE) || joyState.Gamepad.wButtons == XINPUT_GAMEPAD_A 
-		&&leftFlag_ == false && rightFlag_ == false) {
-		Sleep(1 * 300);
-		sceneNo = stageNo_[stageCount_];
-		stageCount_ = 0;
-	}
-
-	worldTransformSunny_.translation_.x = -cosf(rotf[SUNNY]) * 20.0f;
-	worldTransformSunny_.translation_.z = -sinf(rotf[SUNNY]) * 20.0f;
-
-	worldTransformRain_.translation_.x = -cosf(rotf[RAIN]) * 20.0f;
-	worldTransformRain_.translation_.z = -sinf(rotf[RAIN]) * 20.0f;
-
-	worldTransformSnow_.translation_.x = -cosf(rotf[SNOW]) * 20.0f;
-	worldTransformSnow_.translation_.z = -sinf(rotf[SNOW]) * 20.0f;
-
-	worldTransformFog_.translation_.x = -cosf(rotf[FOG]) * 20.0f;
-	worldTransformFog_.translation_.z = -sinf(rotf[FOG]) * 20.0f;
-
-#ifdef _DEBUG
-
-	ImGui::Begin("stageNum");
-
-	float position[3]{
-	    worldTransformSunny_.translation_.x, worldTransformSunny_.translation_.y,
-	    worldTransformSunny_.translation_.z};
-
-	/*float position_2[3]{
-	    worldTransform_2.translation_.x, worldTransform_2.translation_.y,
-	    worldTransform_2.translation_.z};*/
-
-	ImGui::Text("StageCount %d", stageCount_);
-
-	ImGui::Text("rotf %d", leftFlag_);
-
-	ImGui::Text("Speed%d", target_);
-
-	ImGui::Text("%d", summerSound_);
-
-	ImGui::Text("degreeSunny %f", degree[SUNNY]);
-
-	ImGui::Text("degreeRain %f", degree[RAIN]);
-
-	ImGui::Text("degreeSnow %f", degree[SNOW]);
-
-	ImGui::Text("degreeFog %f", degree[FOG]);
-
-	ImGui::SliderFloat3("3DPosition", position, -40.0f, 360.0f);
-
-	ImGui::Text("selectSwitchTimer %f", selectSwitchTimer);
-	ImGui::Checkbox("selectSwitchFlag", &selectSwitchFlag);
-
-	ImGui::End();
-
-#endif
-	// worldTransformSunny_.translation_ = {position[0], position[1], position[2]};
-	worldTransformSunny_.UpdateMatrix();
-	worldTransformRain_.UpdateMatrix();
-	worldTransformSnow_.UpdateMatrix();
-	worldTransformFog_.UpdateMatrix();
 }
 
 void SelectScene::Draw() {
@@ -268,7 +51,7 @@ void SelectScene::Draw() {
 	// 背景スプライト描画前処理
 	Sprite::PreDraw(commandList);
 
-	// Sprite_->Draw();
+	backSceneSprite_->Draw();
 
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
@@ -284,17 +67,6 @@ void SelectScene::Draw() {
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(commandList);
 
-	sunModel_->Draw(worldTransformSunny_, viewProjection_);
-
-	rainModel_->Draw(worldTransformRain_, viewProjection_);
-
-	snowModel_->Draw(worldTransformSnow_, viewProjection_);
-
-	fogModel_->Draw(worldTransformFog_, viewProjection_);
-
-	cloud_->Draw(viewProjection_);
-	skydome_->Draw(viewProjection_);
-
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
@@ -307,6 +79,12 @@ void SelectScene::Draw() {
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(commandList);
 
+	testSceneSprite_->Draw();
+	gameSceneSprite_->Draw();
+
+	tutorialSprite_->Draw();
+	stageSprite_->Draw();
+
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
@@ -316,3 +94,4 @@ void SelectScene::Draw() {
 
 #pragma endregion
 }
+
--- DirectXGame/scene/SelectScene.h
+++ DirectXGame/scene/SelectScene.h
@@ -12,14 +12,6 @@
 #include "stage/cloud/Cloud.h"
 #include <memory>
 
-enum Weather {
-	SUNNY,
-	RAIN,
-	SNOW,
-	FOG,
-	MAX,
-};
-
 class SelectScene : public IScene {
 
 public:
@@ -29,81 +21,26 @@ public:
 
 	void Draw() override;
 
-
 private:
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 
-	WorldTransform worldTransformSunny_;
-	WorldTransform worldTransformRain_;
-	WorldTransform worldTransformSnow_;
-	WorldTransform worldTransformFog_;
+	uint32_t backSceneTexHandle_ = 0;
+	Sprite* backSceneSprite_ = nullptr;
 
-	WorldTransform worldTransformSky_;
+	uint32_t testSceneTexHandle_ = 0;
+	Sprite* testSceneSprite_ = nullptr;
 
-	ViewProjection viewProjection_;
+	uint32_t gameSceneTexHandle_ = 0;
+	Sprite* gameSceneSprite_ = nullptr;
 
-	uint32_t titleTexHandle_ = 0;
-	Sprite* Sprite_ = nullptr;
+	uint32_t tutorialTexHandle_ = 0;
+	Sprite* tutorialSprite_ = nullptr;
 
-	// 晴
-	std::unique_ptr<Model> sunModel_;
-	// 雨
-	std::unique_ptr<Model> rainModel_;
-	// 雪
-	std::unique_ptr<Model> snowModel_;
-	std::unique_ptr<Model> snowHatModel_;
-	// 霧
-	std::unique_ptr<Model> fogModel_;
+	uint32_t stageTexHandle_ = 0;
+	Sprite* stageSprite_ = nullptr;
 
-	// Skydomeの3Dモデル
-	std::unique_ptr<Model> modelSkydome_;
-
-	// スカイドーム
-	std::unique_ptr<Skydome> skydome_;
-
-	// 雲の3Dモデル
-	std::unique_ptr<Model> modelCloud_;
-
-	// 雲
-	std::unique_ptr<Cloud> cloud_;
-
-	// 　矢印を押したときの番号
-	int stageCount_ = 0;
-
-	// 左右のキーを押したときのフラグ
-	bool leftFlag_ = false;
-	bool rightFlag_ = false;
-
-	bool selectSwitchFlag = true;
-	float selectSwitchTimer = 120;
-
-	// 回転の処理
-	float degree[4];
-	float rotf[4];
-
-	// 移動を管理する変数
-	int target_ = 0;
-
-	// 初期ポジション
-	float position_[4] = {90, 180, 270, 360};
-
-	// ステージナンバー
-	int stageNo_[4] = {SUNNYSTAGE, RAINSTAGE, SNOWSTAGE, FOGSTAGE};
-
-	
-	// 読み込みサウンド
-	uint32_t BGM_;
-	uint32_t cloudSound_;
-	uint32_t gameOverSound_;
-	uint32_t gameClearSound_;
-	uint32_t decisionSound_;
-	uint32_t moveSound_;
-	uint32_t summerSound_;
-
-	//サウンドフラグ
-	bool setFlag_ = false;
-	bool summerON = true;
+	int selectCount = 0;
 	
 };
